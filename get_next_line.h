66a8f536df309c7c6163cc9984b746ad64457e93
Samuel Fernandez <43337768+samuelfer42@users.noreply.github.com>
2018-11-30 12:33:28 +0100

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfernand <sfernand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/28 15:34:48 by sfernand          #+#    #+#             */
+/*   Updated: 2018/11/30 11:46:00 by sfernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-# include "libft.h"
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
+# define BUFF_SIZE 1
 # include <unistd.h>
+# include "libft/includes/libft.h"
 
-int get_next_line(const int fd, char **line);
-
+int				get_next_line(const int fd, char **line);
 
 #endif
